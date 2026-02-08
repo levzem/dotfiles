cee30a69b7df3206740d8f295938109ef389f909
Lev Zemlyanov <levzemlyan@gmail.com>
2026-02-08 15:11:09 -0800

    use proper swift formatter

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -673,6 +673,13 @@ require('lazy').setup({
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
+      formatters = {
+        swiftformat = {
+          command = 'swift',
+          args = { 'format' },
+          stdin = true,
+        },
+      },
     },
   },
 
